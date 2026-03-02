332226f9663d5dd772a227709fab568bc5b3cba3
VladSteb237 <vladsteb@mail.ru>
2026-03-03 00:52:27 +0300

    add "force-dynamic" to chat.jsx

--- app/(dashboard)/chat/page.jsx
+++ app/(dashboard)/chat/page.jsx
@@ -5,6 +5,7 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 import React from "react";
+export const dynamic = "force-dynamic";
 
 const ChatPage = () => {
   const queryClient = new QueryClient();
